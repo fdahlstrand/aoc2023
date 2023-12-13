5b64a0d1f44d078b1efc36671dc42189fe871555
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-13 16:02:51 +0100

    Work on day12 part 2 (unfinished)

--- 12/day12.clj
+++ 12/day12.clj
@@ -51,3 +51,72 @@
      (map parse-line)
      (map #(count-alternatives (:springs %) (:expected %)))
      (reduce +))
+
+; Part 2
+(defn make-spring
+  [n]
+  (flatten [\. (repeat n \#) \.]))
+
+(defn is-match?
+  ([a b] (when (or (= a b) (= a \?) (nil? a)) true))
+  ([line spring pos] (every? true? (map #(is-match? %1 %2) (for [x (range (dec pos) (dec (+ pos (count spring))))] (get line x)) spring))))
+
+(defn find-possibilities
+  [line spring]
+  (map (fn [[a _]] a)
+       (filter (fn [[a b]] (when b a))  (map-indexed (fn [a b] [a b])
+                                                     (for [pos (range 0 (count line))]
+                                                       (is-match? line spring pos))))))
+
+(find-possibilities "?" (make-spring 1))
+
+(find-possibilities (subs "???.###" 2) (make-spring 1))
+
+(defn search
+  [input springs]
+  (let [[s & ss] springs
+        ix       (- (count input) (+ (count ss) (reduce + ss)))
+        spring   (make-spring s)
+        possibilities (find-possibilities (subs input 0 ix) spring)]
+    {:input (for [p possibilities]
+              (subs input (min (count input) (+ p s 1))))
+     :ix ix
+     :springs ss
+     :possibilities possibilities}))
+
+(defn rec-search
+  [input springs]
+  (let [[s & ss] springs
+        ix       (- (count input) (+ (count ss) (reduce + ss)))
+        spring   (make-spring s)
+        possibilities (find-possibilities (subs input 0 ix) spring)]
+    (if s
+      (if (empty? possibilities)
+        0
+        (for [p possibilities]
+          (let [next-input (subs input (min (count input) (+ p s 1)))]
+            (rec-search next-input ss))))
+      1)))
+; ------------
+(rec-search "???.###" '(1 1 3))
+(search "???.###" '(1 1 3))
+(search "?.###" '(1 3))
+(search "###" '(3))
+
+; ------------
+(search ".??..??...?##." '(1 1 3))
+
+(search "..??...?##." '(1 3))
+(search ".??...?##." '(1 3))
+(search "...?##." '(1 3))
+(search "..?##." '(1 3))
+
+(search "...?##." '(3))
+(search "..?##." '(3))
+(search "...?##." '(3))
+(search "..?##." '(3))
+
+(search "" '())
+(search "" '())
+(search "" '())
+(search "" '())
