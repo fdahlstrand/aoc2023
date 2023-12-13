e29d4d0f84472d8d4a149d079b2c916567504d2e
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-13 22:17:17 +0100

    Solve day 13 part 2

--- 13/day13.clj
+++ 13/day13.clj
@@ -3,7 +3,7 @@
 
 (defn load-data
   [path]
-  (map #(str/split % #"\n") (str/split (slurp path) #"\n\n")))
+  (map (fn [x] (mapv #(into [] %) (str/split x #"\n"))) (str/split (slurp path) #"\n\n")))
 
 (defn equal?
   [pattern [a b]]
@@ -40,10 +40,37 @@
 
 (defn score
   [pattern]
-  (let [col-scores (reflection-score pattern)
-        row-scores (reflection-score (rotate-pattern pattern))]
-    (+ (reduce + (map #(* 100 %) col-scores)) (reduce + row-scores))))
+  (let [col-score (or (first (reflection-score pattern)) 0)
+        row-score (or (first (reflection-score (rotate-pattern pattern))) 0)]
+    (+ (* 100 col-score) row-score)))
 
 ; Part 1
 (reduce + (map score (load-data "sample.txt")))
 (reduce + (map score (load-data "day13.txt")))
+
+; Part 2
+
+(defn toggle
+  [pattern row col]
+  (let [s (case (get (get pattern row) col)
+            \# \.
+            \. \#)]
+    (assoc pattern row (assoc (get pattern row) col s))))
+
+(defn all-toggled
+  [pattern]
+  (for [row (range 0 (count pattern))
+        col (range 0 (count (get pattern 0)))]
+    (toggle pattern row col)))
+
+(defn toggle-score
+  [pattern]
+  (let [toggled   (all-toggled pattern)
+        orig-col  (or (first (reflection-score pattern)) 0)
+        orig-row  (or (first (reflection-score (rotate-pattern pattern))) 0)
+        col-score (or (first (flatten (remove empty? (map (fn [x] (remove #(= orig-col %) x)) (map #(reflection-score %) toggled))))) 0)
+        row-score (or (first (flatten (remove empty? (map (fn [x] (remove #(= orig-row %) x)) (map #(reflection-score %) (map rotate-pattern toggled)))))) 0)]
+    (+ (* 100 col-score) row-score)))
+
+(reduce + (map toggle-score (load-data "sample.txt")))
+(reduce + (map toggle-score (load-data "day13.txt")))
