02c90e372aef73b151a030b0f75164f4b85b5232
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-14 20:21:55 +0100

    Solve day 14 part 2

--- 14/day14.clj
+++ 14/day14.clj
@@ -5,13 +5,6 @@
   [path]
   (into [] (map #(into [] %) (str/split (slurp path) #"\n"))))
 
-(defn north
-  [sym]
-  (case sym
-    \O 0
-    \. 1
-    \# 2))
-
 (defn rotate-note
   [note]
   (apply mapv vector note))
@@ -24,11 +17,22 @@
   [coll]
   (sort-by #(case %1 \O 0 \. 1 \# 2) coll))
 
+(defn move-right
+  [coll]
+  (sort-by #(case %1 \O 1 \. 0 \# 2) coll))
+
 (defn move-line-left
   [line]
   (->> line
        (partition-by cube-shaped?)
        (map move-left)
+       (reduce into [])))
+
+(defn move-line-right
+  [line]
+  (->> line
+       (partition-by cube-shaped?)
+       (map move-right)
        (reduce into [])))
 
 (defn move-stones-north
@@ -38,9 +42,34 @@
        (map move-line-left)
        (rotate-note)))
 
+(defn move-stones-east
+  [note]
+  (->> note
+       (map move-line-right)))
+
+(defn move-stones-south
+  [note]
+  (->> note
+       (rotate-note)
+       (map move-line-right)
+       (rotate-note)))
+
+(defn move-stones-west
+  [note]
+  (->> note
+       (map move-line-left)))
+
 (defn count-round-stones
   [line]
   (count (filter round-shaped? line)))
+
+(defn spin-cycle
+  [note]
+  (->> note
+       (move-stones-north)
+       (move-stones-west)
+       (move-stones-south)
+       (move-stones-east)))
 
 ; Part 1
 (->> (move-stones-north (load-note "day14.txt"))
@@ -48,3 +77,20 @@
      (reverse)
      (map-indexed #(* (inc %1) %2))
      (reduce +))
+
+; Part 2
+(def notes
+  (loop [note (load-note "day14.txt")
+         notes []]
+    (if (some #(= % note) notes)
+      {:start  (.indexOf notes note)
+       :length (- (count notes) (.indexOf notes note))
+       :notes  notes}
+      (recur (spin-cycle note) (conj notes note)))))
+
+(let [index (+ (:start notes) (mod (- 1000000000 (:start notes)) (:length notes)))]
+  (->> (get (:notes notes) index)
+       (map count-round-stones)
+       (reverse)
+       (map-indexed #(* (inc %1) %2))
+       (reduce +)))
