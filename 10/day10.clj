611b90ad5e46f78fede88a7498e25b8fa226e86f
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-16 19:50:35 +0100

    Cleanup experimental files

--- 10/day10.clj
+++ 10/day10.clj
@@ -2,257 +2,242 @@
   (:require [clojure.string :as str])
   (:require [clojure.set :as set]))
 
-(def sample
-  "7-F7-
-.FJ|7
-SJLL7
-|F--J
-LJ.LJ")
+(defn load-sketch
+  [path]
+  (into [] (map #(into [] %) (str/split (slurp path) #"\n"))))
 
-(def data (slurp "day10.txt"))
+(defn sketch-height
+  [sketch]
+  (count sketch))
 
-(defn symbol-to-pipe [s]
-  (case s
-    \| #{:N :S}
-    \- #{:E :W}
-    \L #{:N :E}
-    \J #{:N :W}
-    \7 #{:S :W}
-    \F #{:S :E}
-    \S #{:start}
-    \. nil))
+(defn sketch-width
+  [sketch]
+  (count (get sketch 0)))
 
-(defn get-map
-  ([m row col] (get (get m row) col))
-  ([m [row col]] (get (get m row) col)))
+(defn get-sketch
+  ([sketch row col]   (get (get sketch row) col))
+  ([sketch [row col]] (get (get sketch row) col)))
 
-(defn move [pn m d]
-  (let [s  (get-map m pn)]
-    (when (and s (or (contains? s d) (contains? s :start)))
-      pn)))
+(defn find-start
+  [sketch]
+  (remove
+   nil?
+   (for [row (range 0 (sketch-height sketch))
+         col (range 0 (sketch-width  sketch))]
+     (when (= \S (get-sketch sketch row col))
+       [row col]))))
 
-(defn move-north [[row col] m] (move [(dec row) col]       m :S))
-(defn move-south [[row col] m] (move [(inc row) col]       m :N))
-(defn move-east  [[row col] m] (move [row       (inc col)] m :W))
-(defn move-west  [[row col] m] (move [row       (dec col)] m :E))
+(defn print-sketch
+  [sketch]
+  (doall
+   (map println
+        (for [row (range 0 (sketch-height sketch))]
+          (str/join
+           (for [col (range 0 (sketch-width sketch))]
+             (get-sketch sketch row col)))))))
 
-(defn get-moves [p m]
-  (partition 2 (remove nil?
-                       (flatten (for [d (get-map m p)]
-                                  (case d
-                                    :start (list (move-north p m)
-                                                 (move-south p m)
-                                                 (move-east  p m)
-                                                 (move-west  p m))
-                                    :N     (move-north p m)
-                                    :S     (move-south p m)
-                                    :W     (move-west  p m)
-                                    :E     (move-east  p m)))))))
+(defn print-outline
+  [sketch {path :path left-outline :left-outline right-outline :right-outline}]
+  (doall
+   (map println
+        (for [row (range 0 (sketch-height sketch))]
+          (str/join
+           (for [col (range 0 (sketch-width sketch))]
+             (cond
+               (contains? path [row col])          \#
+               (contains? left-outline [row col])  \l
+               (contains? right-outline [row col]) \r
+               :else                               \.)))))))
 
-(defn find-start [m]
-  (remove nil?
-          (for [row (range 0 (count m))
-                col (range 0 (count (get m row)))]
-            (when (contains? (get-map m row col) :start)
-              (list row col)))))
+(defn north
+  ([[row col]] [(dec row) col])
+  ([row col]   [(dec row) col]))
 
-(defn parse-map [input]
-  (let [symbols (for [[row s] (map-indexed vector (str/split input #"\n"))]
-                  [row (map-indexed vector s)])]
-    (into [] (for [[_ row] symbols]
-               (into [] (for [[_ s] row]
-                          (symbol-to-pipe s)))))))
+(defn north-east
+  ([[row col]] [(dec row) (inc col)])
+  ([row col]   [(dec row) (inc col)]))
 
-(defn visited? [coll n] (contains? coll n))
+(defn north-west
+  ([[row col]] [(dec row) (dec col)])
+  ([row col]   [(dec row) (dec col)]))
 
-(def pipe-map (parse-map data))
-(find-start pipe-map)
+(defn south
+  ([[row col]] [(inc row) col])
+  ([row col]   [(inc row) col]))
 
-(defn find-loop [m]
-  (let [start (first (find-start m))
-        sec   (first (get-moves start m))]
-    (loop [[n & ns] (list sec)
-           visited  #{start}
-           path [start]]
-      (if n
-        (if (visited? visited n)
-          (recur ns visited path)
-          (recur (concat (get-moves n m) ns) (conj visited n) (conj path n)))
-        path))))
+(defn south-east
+  ([[row col]] [(inc row) (inc col)])
+  ([row col]   [(inc row) (inc col)]))
+
+(defn south-west
+  ([[row col]] [(inc row) (dec col)])
+  ([row col]   [(inc row) (dec col)]))
+
+(defn east
+  ([[row col]] [row (inc col)])
+  ([row col]   [row (inc col)]))
+
+(defn west
+  ([[row col]] [row (dec col)])
+  ([row col]   [row (dec col)]))
+
+(defn from
+  [[from-row from-col] [row col]]
+  (case [(- row from-row) (- col from-col)]
+    [1  0] :from-north
+    [-1  0] :from-south
+    [0  1] :from-west
+    [0 -1] :from-east))
+
+(defn move-start
+  [sketch [row col]]
+  (cond
+    (contains? #{\| \7 \F} (get-sketch sketch (north row col))) (north row col)
+    (contains? #{\- \J \7} (get-sketch sketch (east  row col))) (east  row col)
+    (contains? #{\| \L \J} (get-sketch sketch (south row col))) (south row col)
+    (contains? #{\- \L \F} (get-sketch sketch (west  row col))) (west  row col)))
+
+(defn fix-start
+  [sketch]
+  (let [start     (first (find-start sketch))
+        north     (get-sketch sketch (north start))
+        east      (get-sketch sketch (east  start))
+        south     (get-sketch sketch (south start))
+        west      (get-sketch sketch (west  start))
+        [row col] start
+        pipe      (cond
+                    (and (contains? #{\| \7 \F} north) (contains? #{\| \L \J} south)) \|
+                    (and (contains? #{\- \J \7} east)  (contains? #{\- \L \F} west))  \-
+                    (and (contains? #{\| \7 \F} north) (contains? #{\- \J \7} east))  \L
+                    (and (contains? #{\| \7 \F} north) (contains? #{\- \L \F} west))  \J
+                    (and (contains? #{\- \L \F} west)  (contains? #{\| \L \J} south)) \7
+                    (and (contains? #{\- \J \7} east)  (contains? #{\| \L \J} south)) \F)]
+    (assoc sketch row (assoc (get sketch row) col pipe))))
+
+(defn next-pipe
+  [sketch from now]
+  (case (get-sketch sketch now)
+    \| (case from :from-north (south now) :from-south (north now))
+    \- (case from :from-west  (east  now) :from-east  (west  now))
+    \L (case from :from-north (east  now) :from-east  (north now))
+    \J (case from :from-north (west  now) :from-west  (north now))
+    \7 (case from :from-south (west  now) :from-west  (south now))
+    \F (case from :from-south (east  now) :from-east  (south now))))
+
+(defn walk-path
+  [input]
+  (let [start (first (find-start input))]
+    (loop [now    (move-start input start)
+           prev   start
+           visited #{start}
+           path    [start]]
+      (if (contains? visited now)
+        path
+        (recur (next-pipe input (from prev now) now) now (conj visited now) (conj path now))))))
 
 ; Part 1
-(/ (count (find-loop (parse-map data))) 2)
+(/ (count (walk-path (load-sketch "sample.txt"))) 2)
+(/ (count (walk-path (load-sketch "sample2.txt"))) 2)
+(/ (count (walk-path (load-sketch "sample3.txt"))) 2)
+(/ (count (walk-path (load-sketch "day10.txt"))) 2)
 
 ; Part 2
-(def pipe-loop (find-loop pipe-map))
+(defn left-outline-pipe
+  [sketch pos next-pos]
+  (let [from  (from pos next-pos)]
+    (case (get-sketch sketch pos)
+      \| (case from
+           :from-north #{(east pos)}
+           :from-south #{(west pos)})
+      \- (case from
+           :from-west  #{(north pos)}
+           :from-east  #{(south pos)})
+      \L (case from
+           :from-south #{(south pos) (south-west pos) (west pos)}
+           :from-west  #{(north-east pos)})
+      \J (case from
+           :from-east  #{(south pos) (south-east pos) (east pos)}
+           :from-south #{(north-west pos)})
+      \7 (case from
+           :from-north #{(north pos) (north-east pos) (east pos)}
+           :from-east  #{(south-west pos)})
+      \F (case from
+           :from-west  #{(west pos) (north-west pos) (north pos)}
+           :from-north #{(south-east pos)}))))
 
-(defn dir [delta]
-  (case delta
-    [0  1] :right
-    [0 -1] :left
-    [1  0] :down
-    [-1  0] :up
-    :else   :error))
+(defn right-outline-pipe
+  [sketch pos next-pos]
+  (let [from  (from pos next-pos)]
+    (case (get-sketch sketch pos)
+      \| (case from
+           :from-south #{(east pos)}
+           :from-north #{(west pos)})
+      \- (case from
+           :from-east  #{(north pos)}
+           :from-west  #{(south pos)})
+      \L (case from
+           :from-west  #{(south pos) (south-west pos) (west pos)}
+           :from-south #{(north-east pos)})
+      \J (case from
+           :from-south #{(south pos) (south-east pos) (east pos)}
+           :from-east  #{(north-west pos)})
+      \7 (case from
+           :from-east  #{(north pos) (north-east pos) (east pos)}
+           :from-north #{(south-west pos)})
+      \F (case from
+           :from-north #{(west pos) (north-west pos) (north pos)}
+           :from-west  #{(south-east pos)}))))
 
-(defn follow-path-left [path]
-  (->> (partition 2 1 (conj path (first path)))
-       (map (fn [[[rowa cola] [rowb colb]]] {:pos (list rowb colb) :dir (dir (list (- rowa rowb) (- cola colb)))}))
-       (map (fn [{[row col] :pos d :dir}]
-              (case d
-                :up    (list row (dec  col))
-                :right (list (dec row) col)
-                :down  (list row       (inc col))
-                :left  (list (inc row) col))))))
+(defn find-outline
+  [input]
+  (let [start  (first (find-start input))
+        sketch (fix-start input)]
+    (loop [now           (move-start sketch start)
+           prev          start
+           path          #{start}
+           right-outline (right-outline-pipe sketch start now)
+           left-outline  (left-outline-pipe sketch start now)]
+      (let [nxt (next-pipe sketch (from prev now) now)]
+        (if (contains? path now)
+          {:path          path
+           :right-outline (set/union right-outline (right-outline-pipe sketch prev now))
+           :left-outline  (set/union left-outline (left-outline-pipe sketch prev now))}
+          (recur nxt
+                 now
+                 (conj path now)
+                 (set/union right-outline (right-outline-pipe sketch prev now))
+                 (set/union left-outline (left-outline-pipe sketch prev now))))))))
 
-(defn follow-path-right [path]
-  (->> (partition 2 1 (conj path (first path)))
-       (map (fn [[[rowa cola] [rowb colb]]] {:pos (list rowb colb) :dir (dir (list (- rowa rowb) (- cola colb)))}))
-       (map (fn [{[row col] :pos d :dir}]
-              (case d
-                :up    (list row (inc  col))
-                :right (list (inc row) col)
-                :down  (list row       (dec col))
-                :left  (list (dec row) col))))))
+(defn surround
+  [[row col]]
+  #{[(inc row) col]
+    [(dec row)  col]
+    [row (inc col)]
+    [row (dec col)]})
 
-(defn follow-path-ext [path]
-  (->> (partition 2 1 (conj path (first path)))
-       (map (fn [[[rowa cola] [rowb colb]]] {:pos (list rowb colb) :dir (dir (list (- rowa rowb) (- cola colb)))}))
-       (map (fn [{[row col] :pos d :dir}] {(list row col) d}))))
+(defn invalid?
+  [sketch [row col]]
+  (let [height (sketch-height sketch)
+        width  (sketch-width sketch)]
+    (or (< row 0) (>= row height) (< col 0) (>= col width))))
 
-(defn surround [[row col] width height]
-  (remove nil? (for [x '((0 -1) (0 1) (-1 0) (1 0))]
-                 (let [[dr dc] x
-                       prow      (+ row dr)
-                       pcol      (+ col dc)
-                       p       [prow pcol]]
-                   (when (and (>= prow 0) (>= pcol 0) (< prow height) (< pcol width))
-                     p)))))
-
-(def part2-sample
-  "...........
-.S-------7.
-.|F-----7|.
-.||.....||.
-.||.....||.
-.|L-7.F-J|.
-.|..|.|..|.
-.L--J.L--J.
-...........")
-
-(def part2-sample2
-  ".F----7F7F7F7F-7....
-.|F--7||||||||FJ....
-.||.FJ||||||||L7....
-FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
-....F-J..F7FJ|L7L7L7
-....L7.F7||L7|.L7L7|
-.....|FJLJ|FJ|F7|.LJ
-....FJL-7.||.||||...
-....L---J.LJ.LJLJ...")
-
-(def part2-sample3
-  "FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L")
-
-(defn region [outline path width height]
+(defn flood-fill
+  [sketch path outline]
   (loop [[q & qs] outline
-         r        #{}]
+         fill     #{}]
     (if q
-      (if (or (contains? path q) (contains? r q))
-        (recur qs r)
-        (recur (doall (concat qs (surround q width height))) (conj r q)))
-      r)))
+      (if (or (contains? path q) (contains? fill q) (invalid? sketch q))
+        (recur qs fill)
+        (recur (set/union qs (surround q)) (conj fill q)))
+      fill)))
 
-(defn find-region [input]
-  (let [m      (parse-map input)
-        height (count (str/split input #"\n"))
-        width  (count (first (str/split input #"\n")))
-        start  (find-start m)
-        path   (find-loop m)
-        left   (set (follow-path-left path))
-        right  (set (follow-path-right path))
-        path   (set path)]
-    {:width        width
-     :height       height
-     :left-region  (region left path width height)
-     :right-region (region right path width height)
-     :path         path
-     :left         left
-     :right        right
-     :start        (set start)
-     :all          (set (for [row (range 0 height)
-                              col (range 0 width)]
-                          [row col]))}))
+(def sketch (load-sketch "day10.txt"))
+(def wombat (find-outline sketch))
 
-(def the-map (parse-map part2-sample2))
-(def the-height (count (str/split part2-sample2 #"\n")))
-(def the-width (count (first (str/split part2-sample2 #"\n"))))
-(def the-start (find-start the-map))
-(def the-path (set (find-loop the-map)))
-(def the-outline (set (follow-path (find-loop the-map))))
-(def all-nodes
-  (set (for [row (range 0 the-height)
-             col (range 0 the-width)]
-         (list row col))))
+(print-outline
+ sketch
+ {:path (wombat :path)
+  :right-outline (flood-fill sketch (wombat :path) (wombat :right-outline))
+  :left-outline (flood-fill sketch (wombat :path) (wombat :left-outline))})
 
-(count (let [region (find-region part2-sample2)]
-         (set/difference (:all region) (:path region) (:region region))))
-
-(def the-fill
-  (loop [[q & qs] '([0 0])
-         out      #{}]
-    (if q
-      (if (or (contains? the-path q) (contains? out q))
-        (recur qs out)
-        (recur (concat qs (surround q the-width the-height)) (conj out q)))
-      out)))
-
-(get-map the-map 0 0)
-
-(def the-outline-ext (into {} (follow-path-ext (find-loop the-map))))
-
-(defn print-region [region]
-  (for [row (range 0 (:height region))]
-    (str/join (for [col (range 0 (:width region))]
-                (cond
-                  (contains? (:left-region region) [row col]) \O
-                  (contains? (:right-region region) [row col]) \I
-                  (contains? (:start  region) [row col]) \S
-                  (contains? (:path   region) [row col]) \X
-                  (contains? (:left   region) [row col]) \l
-                  (contains? (:right  region) [row col]) \r
-                  :else                                  \.)))))
-
-(print-region (find-region data))
-
-(let [r (find-region data)]
-  (if (contains? (:left-region r) [0 0])
-    (count (:right-region r))
-    (count (:left-region r))))
-
-(for [row (range 0 (count the-map))]
-  (str/join (for [col (range 0 (count (get the-map 0)))]
-              (cond
-                (contains? (set the-start) [row col]) \S
-                ; (contains? the-path [row col]) \X
-                ; (contains? (set/difference all-nodes the-path the-fill) [row col]) \I
-                (contains? the-outline-ext [row col]) (case (the-outline-ext [row col])
-                                                        :up    \u
-                                                        :down  \d
-                                                        :right \r
-                                                        :left  \l
-                                                        :else  \?)
-                ; (contains? the-fill [row col]) \O
-                (contains? the-outline [row col]) \*
-                ; (contains? the-region [row col]) \A
-                ; (contains? the-path [row col]) \X
-                :else                          \.))))
+(count (flood-fill sketch (wombat :path) (wombat :right-outline)))
