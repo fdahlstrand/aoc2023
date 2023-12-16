0bbf74e74d4aef6929d126cc7e4666209524931c
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-16 12:15:21 +0100

    Solve day 16 part 2

--- 16/day16.clj
+++ 16/day16.clj
@@ -51,11 +51,32 @@
                  :down  [[(inc row) col       :down]]))))
 
 (defn trace-beams
-  [layout]
-  (loop [[b & bs] [[0 0 :right]]
+  [layout start]
+  (loop [[b & bs] [start]
          visited  #{}]
     (if b
       (if (contains? visited b)
         (recur bs visited)
         (recur (concat bs (next-beams layout b)) (conj visited b)))
       visited)))
+
+(defn generate-beams
+  [layout]
+  (concat (for [row (range 0 (height layout))] [row 0 :right])
+          (for [row (range 0 (height layout))] [row (dec (width layout)) :left])
+          (for [col (range 0 (width layout))] [0 col :down])
+          (for [col (range 0 (width layout))] [(dec (height layout)) col :up])))
+
+(defn count-beams
+  [visited]
+  (count (into #{} (map (fn [[row col _]] [row col]) visited))))
+
+(defn optimize-power
+  [layout]
+  (reduce max (map count-beams (map #(trace-beams layout %) (generate-beams layout)))))
+
+; Part 1
+(count-beams (trace-beams (load-layout "day16.txt") [0 0 :right]))
+
+; Part 2
+(optimize-power (load-layout "day16.txt"))
